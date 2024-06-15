87667e73658e1700d848574dfd85db331b68e04c
Ruben Glapa <rubenglapa@gmail.com>
2024-06-15 06:34:35 -0700

    Initial Commit

---
+++ PetWatch/PetWatchApp.swift
@@ -0,0 +1,32 @@
+//
+//  PetWatchApp.swift
+//  PetWatch
+//
+//  Created by Ruben Glapa on 6/15/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct PetWatchApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ PetWatchTests/PetWatchTests.swift
@@ -0,0 +1,16 @@
+//
+//  PetWatchTests.swift
+//  PetWatchTests
+//
+//  Created by Ruben Glapa on 6/15/24.
+//
+
+import Testing
+
+struct PetWatchTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
